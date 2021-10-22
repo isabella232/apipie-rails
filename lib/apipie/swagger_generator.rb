e3ded838fe923b08d4215b19a97eb4e2a007a9da
Huan Ma <huan.ma@instacart.com>
2021-10-22 13:48:45 -0400

    make docs properly

--- lib/apipie/swagger_generator.rb
+++ lib/apipie/swagger_generator.rb
@@ -318,7 +318,7 @@ module Apipie
         dateTime: SwaggerTypeWithFormat.new('string', 'date-time'),
         password: SwaggerTypeWithFormat.new('string', 'password'),
         oneOf: SwaggerTypeWithFormat.new('oneOf', nil),
-        bulk: 'array'
+        bulk: SwaggerTypeWithFormat.new('bulk', nil),
       }
     end
 
@@ -494,6 +494,15 @@ module Apipie
                               else
                                 { type: 'string' }
                               end
+      when 'bulk'
+        swagger_def[:items] = {
+                                '$ref' => gen_referenced_block_from_params_array(
+                                  ref_name_from_param_desc(param_desc),
+                                  param_desc.validator.param_description.validator.params_ordered,
+                                  allow_nulls
+                                )
+                                }
+
 
       when 'enum'
         swagger_def[:type] = 'string'
